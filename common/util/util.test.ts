cdff4d79b916f761079efd83fe5793d19bc47042
ShanaryS <shanaryjs@gmail.com>
2026-09-26 15:40:55 -0400

    add support for displaying token gloss

--- common/util/util.test.ts
+++ common/util/util.test.ts
@@ -1077,6 +1077,7 @@ describe('areTokenizationsEqual', () => {
                 states: ['ignored'],
                 readings: [{ pos: [0, 1], reading: 'a' }],
                 frequency: 1,
+                gloss: 'first',
                 groupingKey: 'group-a',
             },
         ],
@@ -1096,6 +1097,7 @@ describe('areTokenizationsEqual', () => {
                     pos: [...token.pos],
                     states: [...token.states],
                     readings: token.readings.map((reading: any) => ({ ...reading, pos: [...reading.pos] })),
+                    gloss: token.gloss,
                 })),
             })
         ).toBe(true);
